e5bd823cacc43404167bfda935c3e77002b68ad4
NikolaCarovic <nikola.carovic@yahoo.com>
2017-05-21 16:47:12 +0200

    Uvedene sistemske operacije

---
+++ menjacnica/src/sistemske_operacije/SOobrisiValutu.java
@@ -0,0 +1,15 @@
+package sistemske_operacije;
+
+import java.util.LinkedList;
+
+import menjacnica.Valuta;
+
+public class SOobrisiValutu {
+	
+	public static void izvrsi(LinkedList<Valuta> kursnaLista,Valuta valuta){
+		if (!kursnaLista.contains(valuta))
+			throw new RuntimeException("Valuta ne postoji u kursnoj listi");
+		
+		kursnaLista.remove(valuta);
+	}
+}
